7ac938fff183b8af6a3ad69b6d770b629d1fa689
Быков Алексей <alexey-bycov@yandex.ru>
2017-06-23 17:27:23 +0700

    Auto-picking rubick

--- game/dota_addons/rubicks_magick/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/rubicks_magick/scripts/vscripts/addon_game_mode.lua
@@ -1,36 +1,35 @@
--- Generated from template
-
-if CAddonTemplateGameMode == nil then
-	CAddonTemplateGameMode = class({})
+if CRubicksMagickGameMode == nil then
+	CRubicksMagickGameMode = class({})
 end
 
 function Precache( context )
-	--[[
-		Precache things we know we'll use.  Possible file types include (but not limited to):
-			PrecacheResource( "model", "*.vmdl", context )
-			PrecacheResource( "soundfile", "*.vsndevts", context )
-			PrecacheResource( "particle", "*.vpcf", context )
-			PrecacheResource( "particle_folder", "particles/folder", context )
-	]]
+	PrecacheUnitByNameSync("npc_dota_hero_rubick_rubicks_magick", context)
 end
 
--- Create the game mode when we activate
 function Activate()
-	GameRules.AddonTemplate = CAddonTemplateGameMode()
-	GameRules.AddonTemplate:InitGameMode()
+	GameRules.GameMode = CRubicksMagickGameMode()
+	GameRules.GameMode:InitGameMode()
 end
 
-function CAddonTemplateGameMode:InitGameMode()
-	print( "Template addon is loaded." )
+function CRubicksMagickGameMode:InitGameMode()
+	GameRules:SetSameHeroSelectionEnabled(true)
+	GameRules:SetHeroSelectionTime(0.0)
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
+
+	ListenToGameEvent("player_connect_full", Dynamic_Wrap(CRubicksMagickGameMode, "OnConnectFull"), self)
 end
 
--- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
-	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print( "Template addon script is running." )
-	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
-		return nil
-	end
+function CRubicksMagickGameMode:OnConnectFull(keys)
+	local player = PlayerInstanceFromIndex(keys.index + 1)
+    CreateHeroForPlayer("npc_dota_hero_rubick", player)
+    --[[local player = EntIndexToHScript(keys.index + 1)
+    local playerID = player:GetPlayerID()
+    if playerID < DOTA_MAX_PLAYERS then
+        local hero = CreateHeroForPlayer("npc_dota_hero_rubick" , player)
+    end]]
+end
+
+function CRubicksMagickGameMode:OnThink()
+	local state = GameRules:State_Get()
 	return 1
 end
