59e835402490ecff85774a017a898a6cbc301480
Быков Алексей <alexey-bycov@yandex.ru>
2017-06-25 19:56:40 +0700

    Improve right-click moving code

--- game/dota_addons/rubicks_magick/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/rubicks_magick/scripts/vscripts/addon_game_mode.lua
@@ -1,5 +1,8 @@
-if CRubicksMagickGameMode == nil then
-	CRubicksMagickGameMode = class({})
+
+require("move_controller")
+
+if RubicksMagickGameMode == nil then
+	RubicksMagickGameMode = class({})
 end
 
 function Precache( context )
@@ -7,107 +10,38 @@ function Precache( context )
 end
 
 function Activate()
-	GameRules.GameMode = CRubicksMagickGameMode()
+	GameRules.GameMode = RubicksMagickGameMode()
 	GameRules.GameMode:InitGameMode()
 end
 
-function CRubicksMagickGameMode:InitGameMode()
+function RubicksMagickGameMode:InitGameMode()
 	GameRules:SetSameHeroSelectionEnabled(true)
 	GameRules:SetHeroSelectionTime(0.0)
-	GameRules:GetGameModeEntity():SetThink("OnThink", self, "GlobalThink", 0.01)
+	GameRules:SetPreGameTime(20.0)
 
-	ListenToGameEvent("player_connect_full", Dynamic_Wrap(CRubicksMagickGameMode, "OnConnectFull"), self)
+	GameRules:GetGameModeEntity():SetThink("OnThink", self, "GlobalThink", 2)
+	GameRules:GetGameModeEntity():SetThink(OnMoveHeroesThink, "MoveHeroesThink", 1)
 
-	CustomGameEventManager:RegisterListener("me_mm", Dynamic_Wrap(CRubicksMagickGameMode, "OnMouseMove"))
-	CustomGameEventManager:RegisterListener("me_rd", Dynamic_Wrap(CRubicksMagickGameMode, "OnRightDown"))
-	CustomGameEventManager:RegisterListener("me_ru", Dynamic_Wrap(CRubicksMagickGameMode, "OnRightUp"))
-	CustomGameEventManager:RegisterListener("me_ld", Dynamic_Wrap(CRubicksMagickGameMode, "OnLeftDown"))
-	CustomGameEventManager:RegisterListener("me_lu", Dynamic_Wrap(CRubicksMagickGameMode, "OnLeftUp"))
+	ListenToGameEvent("player_connect_full", Dynamic_Wrap(RubicksMagickGameMode, "OnConnectFull"), self)
+
+	CustomGameEventManager:RegisterListener("me_mm", Dynamic_Wrap(RubicksMagickMoveController, "OnMouseMove"))
+	CustomGameEventManager:RegisterListener("me_rd", Dynamic_Wrap(RubicksMagickMoveController, "OnRightDown"))
+	CustomGameEventManager:RegisterListener("me_ru", Dynamic_Wrap(RubicksMagickMoveController, "OnRightUp"))
+	CustomGameEventManager:RegisterListener("me_ld", Dynamic_Wrap(RubicksMagickMoveController, "OnLeftDown"))
+	CustomGameEventManager:RegisterListener("me_lu", Dynamic_Wrap(RubicksMagickMoveController, "OnLeftUp"))
 end
 
 
-playersRightDown = {}
-playersLeftDown = {}
-playersMoveTo = {}
-
-function CRubicksMagickGameMode:OnConnectFull(keys)
+function RubicksMagickGameMode:OnConnectFull(keys)
 	local player = PlayerInstanceFromIndex(keys.index + 1)
     local playerID = player:GetPlayerID()
     local heroEntity = CreateHeroForPlayer("npc_dota_hero_rubick", player)
-    playersRightDown[playerID] = false;
-    playersLeftDown[playerID] = false;
-end
-
-function CRubicksMagickGameMode:OnMouseMove(keys)
-	local heroEntity = PlayerResource:GetPlayer(keys.playerID):GetAssignedHero()
-	local cursorPos = Vector(keys.worldX, keys.worldY, keys.worldZ)
-	if heroEntity then
-		local oldForward = heroEntity:GetForwardVector()
-		local forward = cursorPos - heroEntity:GetAbsOrigin()
-		if #forward < 1 then forward = oldForward end
-
-		if playersLeftDown[keys.playerID] then
-			heroEntity:SetForwardVector(forward)
-		end
-		if playersRightDown[keys.playerID] then
-			heroEntity:SetForwardVector(forward)
-			playersMoveTo[keys.playerID] = cursorPos
-		end
-	end
-end
-
-function CRubicksMagickGameMode:OnRightDown(keys)
-	playersRightDown[keys.playerID] = true
-	local cursorPos = Vector(keys.worldX, keys.worldY, keys.worldZ)
-	playersMoveTo[keys.playerID] = cursorPos
-	local heroEntity = PlayerResource:GetPlayer(keys.playerID):GetAssignedHero()
-	
-	local oldForward = heroEntity:GetForwardVector()
-	local forward = cursorPos - heroEntity:GetAbsOrigin()
-	if #forward < 1 then forward = oldForward end
-	heroEntity:SetForwardVector(forward)
-	heroEntity:StartGesture(ACT_DOTA_RUN)
-end
-
-function CRubicksMagickGameMode:OnRightUp(keys)
-	playersRightDown[keys.playerID] = false
+    heroEntity:SetHullRadius(0)
+    RubicksMagickMoveController:InitPlayer(playerID)
 end
 
 
-function CRubicksMagickGameMode:OnLeftDown(keys)
-	playersLeftDown[keys.playerID] = true
-	local heroEntity = PlayerResource:GetPlayer(keys.playerID):GetAssignedHero()
-	heroEntity:StartGesture(ACT_DOTA_CAST_ABILITY_6)
-	local cursorPos = Vector(keys.worldX, keys.worldY, keys.worldZ)
-
-	local oldForward = heroEntity:GetForwardVector()
-	local forward = cursorPos - heroEntity:GetAbsOrigin()
-	if #forward < 1 then forward = oldForward end
-	heroEntity:SetForwardVector(forward)
-end
-
-function CRubicksMagickGameMode:OnLeftUp(keys)
-	playersLeftDown[keys.playerID] = false
-end
-
-function CRubicksMagickGameMode:OnThink()
+function RubicksMagickGameMode:OnThink()
 	--local state = GameRules:State_Get()
-	for playerID = 0, DOTA_MAX_PLAYERS-1 do
-		if playersMoveTo[playerID] ~= nil then
-			local heroEntity = PlayerResource:GetPlayer(playerID):GetAssignedHero()
-			local moveFrom = heroEntity:GetAbsOrigin()
-			local moveTo = playersMoveTo[playerID]
-			local vec = moveTo - moveFrom
-			local distance = #vec
-			local moveStep
-			if distance < 12.0 then
-				moveStep = vec
-				heroEntity:RemoveGesture(ACT_DOTA_RUN)
-			else
-				moveStep = vec / distance * 12.0
-			end
-			heroEntity:SetAbsOrigin(moveFrom + moveStep)
-		end
-	end
-	return 0.01
+	return 2
 end
--- game/dota_addons/rubicks_magick/scripts/vscripts/move_controller.lua
+++ game/dota_addons/rubicks_magick/scripts/vscripts/move_controller.lua
@@ -1,8 +1,91 @@
-require("addon_game_move.lua")
 
-function RubickMove(args)
-	local moveFrom = args.caster:GetAbsOrigin()
-	local moveTo = playersMoveTo[args.caster:GetPlayerID()]
-	local moveStep = (moveTo - moveFrom):Normalized() * 3.0
-	args.caster:SetAbsOrigin(moveFrom + moveStep)
+if RubicksMagickMoveController == nil then
+	RubicksMagickMoveController = class({})
+end
+
+playersRightDown = {}
+playersLeftDown = {}
+playersMoveTo = {}
+
+function OnMoveHeroesThink()
+	local MOVE_PER_THINK = 11.0
+	for playerID = 0, DOTA_MAX_PLAYERS - 1 do
+		if playersMoveTo[playerID] ~= nil then
+			local heroEntity = PlayerResource:GetPlayer(playerID):GetAssignedHero()
+			local moveFrom = heroEntity:GetAbsOrigin()
+			local vec = playersMoveTo[playerID] - moveFrom
+			local distance = #vec
+			local newOrigin
+			if distance < MOVE_PER_THINK then
+				newOrigin = moveFrom + vec
+				heroEntity:RemoveGesture(ACT_DOTA_RUN)
+				playersMoveTo[playerID] = nil
+			else
+				newOrigin = moveFrom + (vec / distance) * MOVE_PER_THINK
+			end
+			FindClearSpaceForUnit(heroEntity, newOrigin, false)
+		end
+	end
+	return 0.03
+end
+
+function RubicksMagickMoveController:InitPlayer(playerID)
+	playersRightDown[playerID] = false;
+    playersLeftDown[playerID] = false;
+end
+
+function HeroLookAt(heroEntity, targetPos)
+	if heroEntity ~= nil then
+		local oldForward = heroEntity:GetForwardVector()
+		local forward = targetPos - heroEntity:GetAbsOrigin()
+		if #forward < 1 then 	-- prevent from zero-vector
+			forward = oldForward 
+		end
+		heroEntity:SetForwardVector(forward)
+	end
+end
+
+function RubicksMagickMoveController:OnMouseMove(keys)
+	local heroEntity = PlayerResource:GetPlayer(keys.playerID):GetAssignedHero()
+	local cursorPos = Vector(keys.worldX, keys.worldY, keys.worldZ)
+	if playersLeftDown[keys.playerID] then
+		HeroLookAt(heroEntity, cursorPos)
+	end
+	if playersRightDown[keys.playerID] then
+		HeroLookAt(heroEntity, cursorPos)
+		if playersMoveTo[keys.playerID] == nil then
+			heroEntity:StartGesture(ACT_DOTA_RUN)
+		end
+		playersMoveTo[keys.playerID] = cursorPos
+	end
+end	
+
+
+function RubicksMagickMoveController:OnRightDown(keys)
+	playersRightDown[keys.playerID] = true
+
+	local cursorPos = Vector(keys.worldX, keys.worldY, keys.worldZ)
+	playersMoveTo[keys.playerID] = cursorPos
+
+	local heroEntity = PlayerResource:GetPlayer(keys.playerID):GetAssignedHero()
+	HeroLookAt(heroEntity, cursorPos)
+	heroEntity:StartGesture(ACT_DOTA_RUN)
+end
+
+function RubicksMagickMoveController:OnRightUp(keys)
+	playersRightDown[keys.playerID] = false
+end
+
+
+function RubicksMagickMoveController:OnLeftDown(keys)
+	playersLeftDown[keys.playerID] = true
+	local heroEntity = PlayerResource:GetPlayer(keys.playerID):GetAssignedHero()
+	local cursorPos = Vector(keys.worldX, keys.worldY, keys.worldZ)
+
+	HeroLookAt(heroEntity, cursorPos)
+	--heroEntity:StartGesture(ACT_DOTA_CAST_ABILITY_6)
+end
+
+function RubicksMagickMoveController:OnLeftUp(keys)
+	playersLeftDown[keys.playerID] = false
 end
