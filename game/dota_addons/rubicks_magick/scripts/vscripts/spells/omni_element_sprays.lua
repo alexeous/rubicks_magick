5fe007cb08a27aad88e93908d1d7e50b9922fc99
Быков Алексей <alexey-bycov@yandex.ru>
2017-07-31 02:20:22 +0700

    Rework omni water spray

--- game/dota_addons/rubicks_magick/scripts/vscripts/spells/omni_element_sprays.lua
+++ game/dota_addons/rubicks_magick/scripts/vscripts/spells/omni_element_sprays.lua
@@ -92,9 +92,9 @@ function OmniElementSprays:OmniColdSpraySpell(player, power)
 end
 
 
-function OmniElementSprays:OmniSteamSpray(caster, position, radius, ignoreCaster, damage, isWet)
-	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_WATER, damage / 2, ignoreCaster, isWet, 1.0)
-	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_FIRE, damage / 2, ignoreCaster, false, 1.0)
+function OmniElementSprays:OmniSteamSpray(caster, position, radius, isSelfCast, damage, isWet)
+	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_WATER, damage / 2, isSelfCast, isWet, 1.0)
+	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_FIRE, damage / 2, isSelfCast, false, 1.0)
 
 	local particle = ParticleManager:CreateParticle("particles/omni_sprays/omni_steam_spray/omni_steam_spray.vpcf", PATTACH_CUSTOMORIGIN, nil)
 	position.z = position.z + 40
@@ -102,59 +102,48 @@ function OmniElementSprays:OmniSteamSpray(caster, position, radius, ignoreCaster
 	ParticleManager:SetParticleControl(particle, 1, Vector(1, radius - 100, (isWet and 1 or 0)))
 end
 
-function OmniElementSprays:OmniWaterSpray(caster, position, radius, ignoreCaster, doPush)
-	local canPushCaster = ignoreCaster and doPush and not Spells:IsResistantTo(caster, ELEMENT_WATER)
-
-	local knockbackProperties = {
-        center_x = position.x,
-        center_y = position.y,
-        center_z = position.z,
-        duration = 0.35,
-        knockback_duration = 0.35,
-        knockback_height = 40
-    }
+function OmniElementSprays:OmniWaterSpray(caster, position, radius, isSelfCast, doPush)
+	local canPushCaster = isSelfCast and Spells:ResistanceLevelTo(caster, ELEMENT_WATER) < 2
+	
 	local units = FindUnitsInRadius(caster:GetTeamNumber(), position, nil, radius, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_ALL,
 	    DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, true)
 	for _, unit in pairs(units) do
-		if not (unit == caster and ignoreCaster) then
-			if Spells:IsResistantTo(unit, ELEMENT_WATER) then
-				if (unit.shieldElements[1] == ELEMENT_WATER) and (unit.shieldElements[2] == ELEMENT_WATER) and canPushCaster then
-					local unitPos = unit:GetAbsOrigin()
-					local distance = (unitPos - caster:GetAbsOrigin()):Length2D()
-					local knockbackPropertiesSelf = {
-				        center_x = unitPos.x,
-				        center_y = unitPos.y,
-				        center_z = unitPos.z,
-				        duration = 0.35,
-				        knockback_duration = 0.35,
-				        knockback_height = 40,
-				        knockback_distance = radius + 100 - distance
-				    }	
-		    		caster:AddNewModifier(caster, nil, "modifier_knockback", knockbackPropertiesSelf)						
+		if not (unit == caster and isSelfCast) then
+			Spells:ApplyElementDamage(unit, caster, ELEMENT_WATER, 1, true, 1.0)
+			if doPush then
+				if Spells:ResistanceLevelTo(unit, ELEMENT_WATER) < 2 then
+					OmniElementSprays:Push(unit, caster, position, radius)
+			    elseif canPushCaster then
+			    	OmniElementSprays:Push(caster, caster, unit:GetAbsOrigin(), radius)
 				end
-			else
-				Spells:ApplyElementDamage(unit, caster, ELEMENT_WATER, 1, true)
-				if doPush then
-					local distance = (position - unit:GetAbsOrigin()):Length2D()
-					local multiplier = 1
-					if unit.shieldElements ~= nil then
-						if unit.shieldElements[1] == ELEMENT_EARTH then  multiplier = multiplier / 2  end
-						if unit.shieldElements[2] == ELEMENT_EARTH then  multiplier = multiplier / 2  end
-					end
-					knockbackProperties.knockback_distance = (radius + 100 - distance) * multiplier
-		    		unit:AddNewModifier(caster, nil, "modifier_knockback", knockbackProperties)
-		    	end
 	    	end
 		end
 	end
+
 	local particle = ParticleManager:CreateParticle("particles/omni_sprays/omni_water_spray/omni_water_spray.vpcf", PATTACH_CUSTOMORIGIN, nil)
 	position.z = position.z + 40
 	ParticleManager:SetParticleControl(particle, 0, position)
 	ParticleManager:SetParticleControl(particle, 1, Vector(radius / 250 + 0.2, radius, 0))
 end
 
-function OmniElementSprays:OmniFireSpray(caster, position, radius, ignoreCaster, damage)
-	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_FIRE, damage, ignoreCaster, true)
+function OmniElementSprays:Push(target, caster, center, distance)
+	local distanceToTarget = (target:GetAbsOrigin() - center):Length2D()
+	local multiplier = math.pow(0.5, Spells:ResistanceLevelTo(target, ELEMENT_EARTH))
+	local knockbackProperties = {
+		center_x = center.x,
+		center_y = center.y,
+		center_z = center.z,
+		duration = 0.35,
+		knockback_duration = 0.35,
+		knockback_height = 40,
+		knockback_distance = (distance + 100 - distanceToTarget) * multiplier
+	}
+	target:AddNewModifier(caster, nil, "modifier_knockback", knockbackProperties)
+end
+
+
+function OmniElementSprays:OmniFireSpray(caster, position, radius, isSelfCast, damage)
+	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_FIRE, damage, isSelfCast, true)
 
 	local particle = ParticleManager:CreateParticle("particles/omni_sprays/omni_fire_spray/omni_fire_spray.vpcf", PATTACH_CUSTOMORIGIN, nil)
 	position.z = position.z + 20
@@ -163,8 +152,8 @@ function OmniElementSprays:OmniFireSpray(caster, position, radius, ignoreCaster,
 	ParticleManager:SetParticleControl(particle, 1, Vector(radius / 250 + 0.2, radius, 0))
 end
 
-function OmniElementSprays:OmniColdSpray(caster, position, radius, ignoreCaster, damage)
-	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_COLD, damage, ignoreCaster, true)
+function OmniElementSprays:OmniColdSpray(caster, position, radius, isSelfCast, damage)
+	Spells:ApplyElementDamageAoE(position, radius, caster, ELEMENT_COLD, damage, isSelfCast, true)
 
 	local particle = ParticleManager:CreateParticle("particles/omni_sprays/omni_cold_spray/omni_cold_spray.vpcf", PATTACH_CUSTOMORIGIN, nil)
 	position.z = position.z + 40
