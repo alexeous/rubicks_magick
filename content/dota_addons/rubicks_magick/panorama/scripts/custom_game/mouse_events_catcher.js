b13d83c2506d8a736ad23cc34465b01e7ce19444
Быков Алексей <alexey-bycov@yandex.ru>
2017-08-09 22:35:06 +0700

    Fix did not commit mouse_events_catcher.js changes for move controller improvement

--- content/dota_addons/rubicks_magick/panorama/scripts/custom_game/mouse_events_catcher.js
+++ content/dota_addons/rubicks_magick/panorama/scripts/custom_game/mouse_events_catcher.js
@@ -3,9 +3,12 @@ var oldDown = [ false, false, false ];
 
 function cycle() {
 	var cursorPos = GameUI.GetCursorPosition();
+	var eventKeys = generateEventKeys(cursorPos);
+
+	GameEvents.SendCustomGameEventToServer("me_mc", eventKeys);     // mouse cycle event
 	if(cursorPos[0] != oldCursorPos[0] || cursorPos[1] != oldCursorPos[1]) {
 		oldCursorPos = cursorPos;
-		onMouseMove(cursorPos);
+		GameEvents.SendCustomGameEventToServer("me_mm", eventKeys);
 	}
 	for(var i = 0; i < 3; i++) {
 		var down = GameUI.IsMouseDown(i);
@@ -25,9 +28,7 @@ GameUI.SetMouseCallback( function(eventName, arg) {
 } );
 
 
-
-
-function onMouseMove(cursorPos) {
+function generateEventKeys(cursorPos) {
 	var worldXYZ = Game.ScreenXYToWorld(cursorPos[0], cursorPos[1]);
 	var keys = { 
 		"playerID" : Game.GetLocalPlayerID(),
@@ -35,7 +36,7 @@ function onMouseMove(cursorPos) {
 		"worldY" : worldXYZ[1],
 		"worldZ" : worldXYZ[2]
 	};
-	GameEvents.SendCustomGameEventToServer("me_mm", keys);
+	return keys;
 }
 
 const mouseLetters = [ 'l', 'r', 'm' ];
@@ -44,12 +45,5 @@ function onMouseDownOrUp(num, isDown, cursorPos) {
 	var eventMouseLetter = mouseLetters[num];
 	var eventType = isDown ? 'd' : 'u';
 	var eventName =  eventPrefix + eventMouseLetter + eventType;
-	var worldXYZ = Game.ScreenXYToWorld(cursorPos[0], cursorPos[1]);
-	var keys = { 
-		"playerID" : Game.GetLocalPlayerID(),
-		"worldX" : worldXYZ[0],
-		"worldY" : worldXYZ[1],
-		"worldZ" : worldXYZ[2]
-	};
-	GameEvents.SendCustomGameEventToServer(eventName, keys);
+	GameEvents.SendCustomGameEventToServer(eventName, generateEventKeys(cursorPos));
 }
