5b2ae7cc66fc58f84e718bf51abcdcf47424c3ef
Alex <alexey-bycov@yandex.ru>
2019-07-01 19:13:16 +0700

    Remove beam effect applying delay

--- game/dota_addons/rubicks_magick/scripts/vscripts/modifiers/modifier_beam_cast.lua
+++ game/dota_addons/rubicks_magick/scripts/vscripts/modifiers/modifier_beam_cast.lua
@@ -9,17 +9,23 @@ end
 
 function modifier_beam_cast:OnCreated(kv)
     if IsServer() then
+        self.applyEffectOnTargetSetFirst = true
         self.target = nil
         self.power = 1
         self.spellCastTable = self:GetCaster():GetPlayerOwner().spellCast
         self.effectFunction = self.spellCastTable.beams_EffectFunction
         self:StartIntervalThink(0.25)
+        self:ApplyBeamEffect()
     end
 end
 
 function modifier_beam_cast:SetTarget(target)
     if IsServer() then
         self.target = target or self.target
+        if self.applyEffectOnTargetSetFirst then
+            self.applyEffectOnTargetSetFirst = false
+            self:ApplyBeamEffect()
+        end
     end
 end
 
@@ -31,12 +37,16 @@ end
 
 function modifier_beam_cast:OnIntervalThink()
     if IsServer() then
-        if self.target ~= nil then
-            self.effectFunction(self.target, self.power)
-            self.power = self.power + 1
-        elseif self.power > 1 then
-            self.power = self.power - 1
-        end
-        self.spellCastTable.beams_Power = self.power
+        self:ApplyBeamEffect()
     end
+end
+
+function modifier_beam_cast:ApplyBeamEffect()
+    if self.target ~= nil then
+        self.effectFunction(self.target, self.power)
+        self.power = self.power + 1
+    elseif self.power > 1 then
+        self.power = self.power - 1
+    end
+    self.spellCastTable.beams_Power = self.power
 end
